ef4fb90f66d758bd918de9a597dbc3eaca243027
YoanBuscail <yoan.buscail@gmail.com>
2023-07-21 14:11:34 +0200

    atelier S07E02 ajout bonus

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -24,10 +24,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::get('/tasks', [TaskController::class, 'index']);
 // Route::get('/tasks/{id}', [TaskController::class, 'show']);
 
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/{id}', [CategoryController::class, 'show']);
+/* Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']); */
 
-Route::get('/tags', [TagController::class, 'index']);
-Route::get('/tags/{id}', [TagController::class, 'show']);
+/* Route::get('/tags', [TagController::class, 'index']);
+Route::get('/tags/{id}', [TagController::class, 'show']); */
 
 Route::apiResource('tasks', TaskController::class);//->only('index', 'show') pour seulement 2 routes
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('tags', TagController::class);
